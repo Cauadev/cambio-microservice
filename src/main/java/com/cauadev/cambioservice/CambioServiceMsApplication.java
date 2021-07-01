023211826913b76c5f3ef859447d22cfce7e6992
CAUADEV <cauadev@gmail.com>
2021-07-01 15:27:55 -0300

    adding start files of project

---
+++ src/main/java/com/cauadev/cambioservice/CambioServiceMsApplication.java
@@ -0,0 +1,13 @@
+package com.cauadev.cambioservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CambioServiceMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CambioServiceMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cauadev/cambioservice/CambioServiceMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cauadev.cambioservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CambioServiceMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
